74cde87d6d27fd39d6974ff9d55aff01ba6f0c3e
testspar3 <testspar3@gmail.com>
2017-03-01 16:26:52 +0530

    revert changes

--- src/com/vaannila/domain/User.java
+++ src/com/vaannila/domain/User.java
@@ -5,7 +5,6 @@ public class User {
 	private String name;
 	private String password;
 	private String gender;
-	private String gender1;
 	private String country;
 	private String aboutYou;
 	private String[] community;
